31cd4c1c2fdfec8418f32781dc600aad8cf6543d
Robert Jackson <rjackson@linkedin.com>
2020-09-14 11:19:22 -0400

    Add another basic pretender test (matching QPs).

--- tests/unit/basic-pretender-test.js
+++ tests/unit/basic-pretender-test.js
@@ -21,4 +21,35 @@ module('basic pretender functionality', function (hooks) {
 
     assert.deepEqual(await result.json(), { id: '3' });
   });
+
+  test('can match a get request with specific query params independently', async function (assert) {
+    this.server.get('/api/graphql', (request) => {
+      switch (request.queryParams.foo) {
+        case 'bar':
+          return [200, {}, JSON.stringify({ query: 'bar' })];
+        case 'baz':
+          return [200, {}, JSON.stringify({ query: 'baz' })];
+      }
+    });
+
+    let result = await fetch('/api/graphql?foo=baz');
+    assert.deepEqual(await result.json(), { query: 'baz' }, 'can access baz');
+
+    result = await fetch('/api/graphql?foo=bar');
+    assert.deepEqual(await result.json(), { query: 'bar' }, 'can access bar');
+
+    await assert.rejects(
+      (async () => {
+        result = await fetch('/api/graphql?foo=derp');
+
+        assert.deepEqual(
+          await result.json(),
+          { query: 'bar' },
+          'can access bar'
+        );
+      })(),
+      /Nothing returned by handler for \/api\/graphql\?foo=derp/,
+      'throws when missing a match'
+    );
+  });
 });

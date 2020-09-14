b8ebc9c42ce95cadc4d57919302b189c642b7409
Robert Jackson <rjackson@linkedin.com>
2020-09-14 09:24:02 -0400

    Add prettier configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,8 @@ module.exports = {
   ],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended'
   ],
   env: {
     browser: true
